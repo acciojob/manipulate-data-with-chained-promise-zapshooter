ea282981234ab580b2624ea329e83ea1bcf665e6
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-04-22 22:52:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-//your JS code here. If required.
-<!DOCTYPE html>
+function processArray(arr) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(arr);
+    }, 3000);
+  });
+}
 
-<html>
-  <head>
-    <link rel="stylesheet" href="styles.css" />
-  </head>
-
-  <body>
-    <!-- do not change this code -->
-    <div id="output"></div>
-    <script type="text/javascript" src="./script.js"></script>
-  </body>
-</html>
+processArray([1, 2, 3, 4])
+  .then((arr) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const evens = arr.filter((n) => n % 2 === 0);
+        document.getElementById("output").textContent = evens.join(",");
+        resolve(evens);
+      }, 1000);
+    });
+  })
+  .then((evens) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const doubled = evens.map((n) => n * 2);
+        document.getElementById("output").textContent = doubled.join(",");
+        resolve(doubled);
+      }, 2000);
+    });
+  });
